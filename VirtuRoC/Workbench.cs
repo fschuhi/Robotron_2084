e6f0d4ea75e92f9b93d8256fbb6897238c4a0e59
Frank Schuhardt <frank.schuhardt@gmail.com>
2020-08-20 11:00:48 +0200

    delegates for read/write/execute recording

--- VirtuRoC/Workbench.cs
+++ VirtuRoC/Workbench.cs
@@ -10,18 +10,111 @@ using CommandLine;
 using System.Runtime.InteropServices;
 using FastColoredTextBoxNS;
 using System.Windows.Forms;
+using CsvHelper;
+using System.Globalization;
+using System.Threading;
+using System.Linq;
 
 // TODO: how to use the Asm-Button on the MainPage? Probably an event...
 
+
 namespace Robotron {
 
+    #region recording
+    public enum RecordedOperationType { Executed, Read, Write }
+
+    public class RecordedOperation {
+        public long Cycles { get; set; }
+        public RecordedOperationType Type { get; set; }
+        public long OpcodeRPC { get; set; }
+    }
+
+    public class RecordedMemoryOperation : RecordedOperation {
+        public long Address { get; set; }
+        public int Value { get; set; }
+        public int OldValue { get; set; }
+    }
+
+    public class RecordedExecutedOperation : RecordedOperation {
+        public int Opcode { get; set; }
+        public int EA { get; set; }
+        public int RPC { get; set; }
+        public int RA { get; set; }
+        public int RX { get; set; }
+        public int RY { get; set; }
+        public int RP { get; set; }
+        public int RS { get; set; }
+    }
+
+    public class RecordedOperations {
+
+        public List<RecordedOperation> Operations = new List<RecordedOperation>();
+        Cpu _cpu;
+
+        public RecordedOperations( Machine machine ) {
+            _cpu = machine.Cpu;
+        }
+
+        public void Store( RecordedOperationType type, int address, int value, int oldValue = 0 ) {
+            Operations.Add( new RecordedMemoryOperation() {
+                Cycles = _cpu.Cycles,
+                OpcodeRPC = _cpu.OpcodeRPC,
+                Type = type,
+                Address = address,
+                Value = value,
+                OldValue = type == RecordedOperationType.Write ? oldValue : value,
+            } );
+        }
+
+        public void Read( int address, int value ) {
+            Store( RecordedOperationType.Read, address, value );
+        }
+
+        public void Write( int address, int value, int oldValue ) {
+            Store( RecordedOperationType.Write, address, oldValue );
+
+            // TODO: hier delegates (oder events? nochmal das GC Thema suchen), RecordedOperations -> Workbench
+            // TODO: track opcodes (R..., auch OpcodeRPC und Cycles)
+            // TODO: breakpoint @ cycles => MessageBox in regelmäßigen Abständen, damit wir uns nicht totsammeln
+        }
+
+        public void Executed( Cpu cpu ) {
+            Operations.Add( new RecordedExecutedOperation() {
+                Cycles = _cpu.Cycles,
+                OpcodeRPC = _cpu.OpcodeRPC,
+                Type = RecordedOperationType.Executed,
+                Opcode = _cpu.OpCode,
+                EA = _cpu.EA,
+                RPC = _cpu.RPC,
+                RA = _cpu.RA,
+                RX = _cpu.RX,
+                RY = _cpu.RY,
+                RP = _cpu.RP,
+                RS = _cpu.RS,
+            } );
+        }
+
+        public IEnumerable<RecordedMemoryOperation> MemoryOperations {
+            get { return Operations.Where( x => x is RecordedMemoryOperation ).Select( x => x as RecordedMemoryOperation ); }
+        }
+        public IEnumerable<RecordedExecutedOperation> ExecutedOperations {
+            get { return Operations.Where( x => x is RecordedExecutedOperation ).Select( x => x as RecordedExecutedOperation ); }
+        }
+    }
+    #endregion
+    
+    
     class Workbench : RobotronObject {
 
         public MachineOperator _mo;
+        public Memory _mem;
+        public Cpu _cpu;
 
         public AsmService AsmService { get; private set; }
         public Options _options;
         WorkbenchScript1 _script;
+
+        public RecordedOperations RecordedOperations;
 
         AsmLinesWindow _window1;
 
@@ -32,6 +125,18 @@ namespace Robotron {
             _mo.OnPaused += mo_OnPause;
             _options = options;
             AsmService = new AsmService();
+            RecordedOperations = new RecordedOperations( _mo.Machine );
+
+            _mem = _mo.Machine.Memory;
+            _cpu = _mo.Machine.Cpu;
+
+            /*
+            _mem.OnRead += mem_OnRead;
+            _mem.OnReadZeroPage += mem_OnRead;
+            _mem.OnWrite += mem_OnWrite;
+            _mem.OnWriteZeroPage += mem_OnWrite;
+            _cpu.OnExecuted += cpu_OnExecuted;
+            */
         }
 
         private void DoAction() {
@@ -59,12 +164,39 @@ namespace Robotron {
         private void mo_OnPause( MachineOperator mo, PausedEventArgs e ) {
             _script.mo_OnPause( mo, e );
 
-            TraceLine( "opcount", _mo.Machine.Memory.RecordedOperations.Operations.Count );
+            /*
+            TraceLine( $"executed: {_executed}, reads: {_reads}, writes: {_writes}" );
+            using (var writer = new StreamWriter( @"s:\source\repos\Robotron_2084\VirtuRoC\tmp\Operations.csv" ))
+            using (var csv = new CsvWriter( writer, Thread.CurrentThread.CurrentCulture )) {
+                csv.WriteRecords( RecordedOperations.MemoryOperations );
+                //csv.WriteRecords( RecordedOperations.ExecutedOperations );
+            }
+            */
 
             _window1.ScrollToAddress( e.BreakpointRCP );
 
             // TODO: Warum brauchen wir hier auch ein MainPage.Focus() ?
             _mo.MainPage.Focus();
+        }
+
+
+        long _reads;
+        long _writes;
+        long _executed;
+
+        private void mem_OnRead( int address, int value ) {
+            _reads++;
+            RecordedOperations.Read( address, value );
+        }
+
+        private void mem_OnWrite( int address, int value, int oldValue ) {
+            _writes++;
+            RecordedOperations.Write( address, value, oldValue );
+        }
+
+        private void cpu_OnExecuted( Cpu cpu ) {
+            _executed++;
+            RecordedOperations.Executed( cpu );
         }
 
     }
